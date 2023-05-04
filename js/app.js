8750b9c32fd9a0c2017261043c9bb117df220b05
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-05-04 19:05:16 -0300

    optimizing code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
-$('textarea').on('keyup', function () {
-  let valueTextArea = $('textarea').val().length,
-    words = $('textarea').val().split(' ').length;
-  $('.caracteres').text(valueTextArea);
-  $('.palavras').text(words);
+$('textarea').on('keyup paste', function () {
+  setTimeout(function () {
+    let valueTextArea = $('textarea').val().length,
+      words = $('textarea').val().split(' ').length;
+    $('.caracteres').text(valueTextArea);
+    $('.palavras').text(words);
+  }, 200);
 });
