83f851d6519ce1856f4e6e065b5541c8b143a194
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-05-04 19:02:37 -0300

    implementing function to get the words in the textarea

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 $('textarea').on('keyup', function () {
-  let valueTextArea = $('textarea').val().length;
+  let valueTextArea = $('textarea').val().length,
+    words = $('textarea').val().split(' ').length;
   $('.caracteres').text(valueTextArea);
+  $('.palavras').text(words);
 });
